b8b0fbd25a5f6598e031ec9599d63f8e0956b0bf
SangHoon Lee <realdevelope@naver.com>
2021-11-26 18:33:59 +0900

    feat : MainPage, MapPage, Footer 추가

--- Front/yososu_map/src/App.js
+++ Front/yososu_map/src/App.js
@@ -2,18 +2,16 @@ import React from 'react';
 import MapPage from './components/MapPage/MapPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col} from "react-bootstrap";
+import MainPage from './components/MainPage/MainPage';
+import Footer from '../src/components/Footer/Footer'
+
 
 function App() {
   return (
     <div>
-      <Container>
-        <Row>
-          <Col>
-            <MapPage/>
-          </Col>
-        </Row>
-      </Container>
-
+      <MainPage/>
+      <MapPage/>
+      <Footer/>
     </div>
   );
 }
