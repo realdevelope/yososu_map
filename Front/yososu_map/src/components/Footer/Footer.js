9f196820e653972531a9516783f42ed02d647156
SangHoon Lee <realdevelope@naver.com>
2021-11-26 18:38:02 +0900

    feat : footer추가

---
+++ Front/yososu_map/src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="page-footer font-small blue pt-4">
+                <div className="container-fluid text-center bg-black pt-3">
+                    <div className="row">
+                        <div className="col">
+                            <h5 className="text-uppercase text-white">SSAC(Seoul SoftWare Academy Cluster)</h5>
+                            <p className="text-white">SSAC3기 - 웹 풀스택 융합 개발자 양성과정</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-copyright text-center py-2 text-white bg-black">© 2021 Copyright:
+                    <a className="text-white" href="https://github.com/kebin0613/SSAC_Team/"> SSAC Team</a>
+                </div>
+
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
