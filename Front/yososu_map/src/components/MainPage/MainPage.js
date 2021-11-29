87e9b5c6911340ccf7cb6b4c20a2ca32b8ff38a9
SangHoon Lee <realdevelope@naver.com>
2021-11-29 12:38:33 +0900

    fix : react typing -> static text로 변경

--- Front/yososu_map/src/components/MainPage/MainPage.js
+++ Front/yososu_map/src/components/MainPage/MainPage.js
@@ -1,40 +1,26 @@
-import React, { useState, useEffect } from 'react'
-import { Image } from 'react-bootstrap';
+import React from 'react'
+import { Image, Row, Col } from 'react-bootstrap';
 import Navigation from '../Navigation/Navigation';
-import "./MainPage.css";
 
 
 const MainPage = () => {
 
-    const txt = `요소수 재고, 위치, 정보, 이슈를 
-요소수 맵에서 한눈에 확인 하세요.`;
-
-    const [Text, setText] = useState('');
-    const [Count, setCount] = useState(0);
-    
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setText(Text + txt[Count]); // 이전 set한 문자 + 다음 문자
-            setCount(Count + 1); // 개수 만큼 체크 
-        }, 100);
-
-        if(Count === txt.length)  {  // Count를 따로 두지 않고 Text.length 체크도 가능
-            clearInterval(interval); // 문자열 체크를 통해 setInterval을 해제합니다
-        }
-        return () => clearInterval(interval); // 언마운트시 setInterval을 해제합니다
-    })
-
     return (
         <div>
             <Navigation/>
-            <div class="lb-wrap">
-                <div class="lb-text">
-                    <pre id="title">{ Text }|</pre>
-                </div>
-                <div className="MemberInfo my-4">
-                    <Image src="main.PNG" style={{height:'auto',width:'100%', paddingBottom:'50px'}}/>
-                </div>
-            </div>
+                <Row>
+                    <Col>
+                    <div className="my-5">
+                        <Image src="main.PNG" style={{ height:'100%', width:'100%', position: 'relative' }}/>
+                    </div>
+
+                    <div className="col-md-5" style={{ position:'absolute', top:'35%', left:'15%', color:'white' }}>
+                        <h1 class="mb-3">요소수 판매처와 재고, 정보, 최신뉴스를 Yososu Map에서 한눈에 확인 하세요!</h1>
+                        <p>저희 000은 어떠한 영리도 취하지 않고, 여러분 모두에게 큐레이션 서비스를 제공합니다. </p>
+                    </div>
+                    </Col>
+                </Row>
+                
         </div>
     )
 }
