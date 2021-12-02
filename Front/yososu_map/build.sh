2c78dd141e5ad2e351ec16afc71ee3b14d50ac38
SangHoon Lee <realdevelope@naver.com>
2021-12-03 00:26:28 +0900

    fix: build script 수정

--- Front/yososu_map/build.sh
+++ Front/yososu_map/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # shellcheck disable=SC2046
-echo `yarn install`
-echo `yarn build`
+#echo `yarn install`
+#echo `yarn build`
+
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=api-proxy:0.1)) || notExistsUploade=false
+docker rmi api-proxy:0.1
 
 echo `docker build -t api-proxy:0.1 ../../Back/proxy`
 echo `docker run -d -p 3001:3001 api-proxy:0.1`
