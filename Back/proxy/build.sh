b86bda2889b48dd6f553af4124d462dde0330fb6
SangHoon Lee <realdevelope@naver.com>
2021-12-06 13:46:41 +0900

    fix : Proxy 서버 인스턴스 변경으로 인한 수정

--- Back/proxy/build.sh
+++ Back/proxy/build.sh
@@ -1,5 +1,5 @@
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=api-proxy:0.1)) || notExistsUploade=false
+docker rm $(docker stop $(docker ps -a -q —filter ancestor=api-proxy:0.1)) || notExistsUploade=false
 docker rmi api-proxy:0.1
 
 echo `docker build -t api-proxy:0.1 .`
-echo `docker run -d -p 80:80 api-proxy:0.1`
+echo `docker run -d -p 3001:3001 api-proxy:0.1`
